cef04ce4418c90e2bb6f5ef145e7d3736becc583
Moinkhank <moin57057@gmail.com>
2024-11-11 12:22:55 +0530

    ok

---
+++ back-end/src/main/java/codingtechniques/SocialAppApplication.java
@@ -0,0 +1,14 @@
+package codingtechniques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringBootApplication
+public class SocialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialAppApplication.class, args);
+	}
+
+}
